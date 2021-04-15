86a212d8e9be6dd0a84e724006397fa9402bbded
COOKIE <69305087+COOKIE1816@users.noreply.github.com>
2021-04-16 00:21:57 +0200

    Create ecdec.js

---
+++ ecdec.js
@@ -0,0 +1,29 @@
+function hide(key) {
+	var x = key;
+	var x = x.replace("0","G");
+	var x = x.replace("1","f");
+	var x = x.replace("2","r");
+	var x = x.replace("3","n");
+	var x = x.replace("4","d");
+	var x = x.replace("5","k");
+	var x = x.replace("6","m");
+	var x = x.replace("7","q");
+	var x = x.replace("8","Q");
+	var x = x.replace("9","J");
+	var out = x;
+}
+
+function show(key) {
+	var z = key;
+	var z = x.replace("G","0");
+	var z = x.replace("f","1");
+	var z = x.replace("r","2");
+	var z = x.replace("n","3");
+	var z = x.replace("d","4");
+	var z = x.replace("k","5");
+	var z = x.replace("m","6");
+	var z = x.replace("q","7");
+	var z = x.replace("Q","8");
+	var z = x.replace("J","9");
+	var out = z;
+}
