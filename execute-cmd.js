9d8c9803df3abd636c8453c60e02786734eafa2c
COOKIE <69305087+COOKIE1816@users.noreply.github.com>
2021-04-16 00:54:22 +0200

    Update execute-cmd.js

--- execute-cmd.js
+++ execute-cmd.js
@@ -23,9 +23,23 @@ function com(command,arg1,arg2,arg3,arg4,arg5,arg6){
 			
 			window.location = "shshutdown.html";
 		}
+		
+		if (arg1 = "-c"){
+			if (arg2 = "-memt"){
+				if (arg3 = "-i"){
+					output("Can't use argument -i on position: 3\n-memt argument on position 2 means memory test!"); 
+
+				}
+				window.location = "shmemtrestart.html";
+			}
+
+			if (arg2 = "-i"){
+				window.location = "shirestart.html"
+			}
+		}
 	}
 	if (command = "memt") {
-		com("restart","-d","-memt");
+		com("restart","-c","-memt");
 	}
 	if (command = "mem") {
 		if (arg1 = "-w") {
